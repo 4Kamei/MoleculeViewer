07b65259145ae8d11cd6f5d25c4945d038003721
Aleksander Kaminski <akaminski2010@gmail.com>
2015-09-20 18:13:12 +0100

    Functional Group Classes - Oh dear that's a lot of code to write

---
+++ src/ak/api/moleculeviewer/functionalgroup/CarboxylicAcidGroup.java
@@ -0,0 +1,7 @@
+package ak.api.moleculeviewer.functionalgroup;
+
+/**
+ * Created by Aleksander Kaminski {theinfamouspig@gmail.com} on 20/09/2015.
+ */
+public class CarboxylicAcidGroup {
+}
---
+++ src/ak/api/moleculeviewer/functionalgroup/FunctionalGroup.java
@@ -0,0 +1,8 @@
+package ak.api.moleculeviewer.functionalgroup;
+
+/**
+ * Created by Aleksander Kaminski {theinfamouspig@gmail.com} on 20/09/2015.
+ */
+public class FunctionalGroup {
+
+}
